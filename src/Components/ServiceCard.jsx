670a5650f7cd135dcd3f8cb16ce9726d0c20e9f9
Akash Kumar Singh <itsakash18.06@gmail.com>
2024-08-24 13:41:04 +0530

    Pricing and service page updated

--- src/Components/ServiceCard.jsx
+++ src/Components/ServiceCard.jsx
@@ -1,24 +1,23 @@
 import React from 'react';
-import icon from '../assets/icon2.svg';
-import service from '../assets/service2.png';
+import { Link } from 'react-router-dom'
 
-const ServiceCard = () => {
+const ServiceCard = ({ data }) => {
     return (
-        <div className="relative bg-white rounded-[1rem] shadow-lg overflow-hidden max-w-[18rem]  group  hover:shadow-xl">
-            <img src={service} alt="Service" className="w-full translate-transform duration-700 group-hover:scale-[1.15] h-auto" />
+        <Link to={data?.link} className="relative bg-white rounded-[1rem] shadow-lg overflow-hidden max-w-[18rem]  group  hover:shadow-xl">
+            <img src={data?.image} alt="Service" className="w-full translate-transform duration-700 group-hover:scale-[1.15] h-auto" />
             <div className="absolute inset-0 bg-gradient-to-t from-[#040D43] via-transparent to-transparent flex items-end justify-center hover:from-[10%] hover:from-[#2B4DFF]  hover:to-transparent transition-all duration-300">
                 <div className="text-center mb-7 text-white relative z-10">
                     <div className="mb-10 bg-[#2B4DFF] p-4 mx-auto w-fit rounded-lg" >
-                        <img src={icon} alt="Icon" className="w-8 h-auto mx-auto transition-colors duration-300 group-hover:text-[#2B4DFF]"
+                        <img src={data?.icon} alt="Icon" className="w-8 h-auto mx-auto transition-colors duration-300 group-hover:text-[#2B4DFF]"
                             style={{ color: 'currentColor' }} />
 
                     </div>
                     <a href="/single-service" tabIndex="0" className="text-[1.4rem] sora-600 leading-tight">
-                        <h5>Web &amp; Mobile App <br /> Development</h5>
+                        <h5 dangerouslySetInnerHTML={{ __html: data?.title }} />
                     </a>
                 </div>
             </div>
-        </div>
+        </Link>
     );
 };
 
--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -7,18 +7,49 @@ import ProjectCard from '../Components/ProjectCard'
 import ContactSection from '../Components/ContactSection'
 import TeamCarousel from '../Components/TeamSection'
 import WebsiteDevelopmentCard from '../Components/WebsiteDevelopmentCard'
+import icon1 from '../assets/icon1.svg'
+import icon2 from '../assets/icon2.svg'
+import icon3 from '../assets/icon3.svg'
+import icon4 from '../assets/icon4.svg'
+import service1 from '../assets/service1.png'
+import service2 from '../assets/service2.png'
+import service3 from '../assets/service3.png'
+import service4 from '../assets/service4.png'
 
 const Home = () => {
+
+    const data = [
+        {
+            title: "Ecommerce Setup <br/> Services",
+            icon: icon1,
+            image: service1,
+            link: "/"
+        },
+        {
+            title: "Website Development <br/> Services",
+            icon: icon2,
+            image: service2,
+            link: "/"
+        }, {
+            title: "Graphic Designing <br/> Services",
+            icon: icon3,
+            image: service3,
+            link: "/"
+        }, {
+            title: `Digital Marketing <br/> Services`,
+            icon: icon4,
+            image: service4,
+            link: "/"
+        },
+    ]
+
     return (
         <div className=''>
             <HeroSection />
             <div className='flex flex-col items-center justify-center mt-20 lg:mt-28 mb-10'>
                 <h1 className='sora-700 leading-[3rem] text-center mb-4 text-[2.2rem] md:text-[3rem] md:leading-[3.3rem] max-w-[25rem] sm:max-w-full text-[#0A165D]'>Our <br className='md:hidden' /> awesome services to <br className='hidden sm:block' /> give you success</h1>
                 <div className='grid-1 grid sm:grid-cols-2 lg:grid-cols-4 gap-8 mt-4'>
-                    <ServiceCard />
-                    <ServiceCard />
-                    <ServiceCard />
-                    <ServiceCard />
+                    {data?.map((dat, ind) => <ServiceCard key={ind} data={dat} />)}
                 </div>
             </div>
             <WebsiteDevelopmentCard />
